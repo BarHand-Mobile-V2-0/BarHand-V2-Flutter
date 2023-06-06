a4dac5862a5b690ccae478efdd5eaf063fe536b7
Ryllex <epostigo1999@gmail.com>
2023-06-06 16:45:23 -0500

    Plantilla

---
+++ lib/sign-in.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class SignInView extends StatefulWidget {
+  const SignInView({Key? key}) : super(key: key);
+
+  @override
+  State<SignInView> createState() => _SignInViewState();
+}
+
+class _SignInViewState extends State<SignInView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
